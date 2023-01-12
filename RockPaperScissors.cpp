7acdc757eac17c7dda99f1c1c928525c3e67827a
Generally-Fluft <98135050+Generally-Fluft@users.noreply.github.com>
2023-01-12 16:11:55 -0600

    Update RockPaperScissors.cpp

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -2,12 +2,34 @@
 #include <random>
 #include <string>
 
+/* 
+ * Generates a random integer between start and end
+ * 
+ * Parameters:
+ * Start: The smallest int to be generated
+ * End: The largest int to be generated
+ * 
+ * Return:
+ * An integer between start and end
+ */
 int RandI(int start, int end) {
     static std::mt19937 prng(std::random_device{}());
     static std::uniform_int_distribution<int> dist(start, end);
     return dist(prng);
 }
 
+/*
+ * Checks if a string is in a given array of strings
+ * 
+ * Parameters: 
+ * str: The string to be matched
+ * array: The array of strings to be checked
+ * size: The size of the array
+ *
+ * Return:
+ * True if str is an element of array
+ * False if str is not an element of array
+ */
 bool isIn(const std::string& str, std::string array[], const int size) {
     for (int s = 0; s < size; s++) {
         if (array[s] == str)
